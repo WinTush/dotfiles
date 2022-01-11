738cf01169232da47d5652d5d347234ddc647edb
Winston Kinoti <prog.kinoti@gmail.com>
2022-01-11 11:47:14 +0300

    Changed keymaps to mappings and added comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+-- You will need to install language servers `npm i -g vscode-langservers-extracted` and `npm install -g typescript typescript-language-server`
+
+-- Plugin management via Packer
 require("plugins")
+-- Vim mappings, see lua/config/which.lua for more mappings
+require("mappings")
+-- All non plugin related (vim) options
 require("options")
-require("keymaps")
+-- Vim autocommands/autogroups
+require("autocmd")
