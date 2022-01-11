0c3030a5b3e52fa8f79d3d917fae29558707328b
Winston Kinoti <prog.kinoti@gmail.com>
2022-01-11 11:52:40 +0300

    Added Go and Exa to fish path

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+# Go
+set -gx PATH /usr/local/go/bin $PATH
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# Exa
+set -gx PATH /usr/local/bin/exa/bin $PATH
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
