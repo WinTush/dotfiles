ba09122c2f38d6b5445050f4aa81782ac8aed400
Winston Kinoti <prog.kinoti@gmail.com>
2022-02-27 18:50:06 +0300

    Tmux clipboard config

--- fish/config.fish
+++ fish/config.fish
@@ -49,3 +49,4 @@ thefuck --alias | source
 set -gx CC "clang"
 set -gx CFLAGS "-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow"
 set -gx LDLIBS "-lcrypt -lcs50 -lm"
+set -gx LD_LIBRARY_PATH /usr/local/lib
