c46ffe63f26149b167b5e64e1178acd8261afe76
Winston Kinoti <prog.kinoti@gmail.com>
2022-06-01 21:57:07 +0300

    Added minimap to lvim.

--- lvim/config.lua
+++ lvim/config.lua
@@ -227,6 +227,16 @@ lvim.plugins = {
         requires = "hrsh7th/nvim-cmp",
         -- event = "InsertEnter",
     },
+    {
+        'wfxr/minimap.vim',
+        run = "cargo install --locked code-minimap",
+        -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+        config = function()
+            vim.cmd("let g:minimap_width = 10")
+            vim.cmd("let g:minimap_auto_start = 1")
+            vim.cmd("let g:minimap_auto_start_win_enter = 1")
+        end,
+    },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
