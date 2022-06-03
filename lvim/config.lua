0723631c87df93220c432c6856a58ca9c0d3070f
Winston Kinoti <prog.kinoti@gmail.com>
2022-06-03 13:33:46 +0300

    Hop plugin for lunarvim.

--- lvim/config.lua
+++ lvim/config.lua
@@ -237,6 +237,15 @@ lvim.plugins = {
             vim.cmd("let g:minimap_auto_start_win_enter = 1")
         end,
     },
+    {
+        "phaazon/hop.nvim",
+        event = "BufRead",
+        config = function()
+            require("hop").setup()
+            vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+            vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+        end,
+    },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
