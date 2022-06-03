a858db7909963ae66de39a5beb81c720f5e2688d
Winston Kinoti <prog.kinoti@gmail.com>
2022-06-03 14:39:09 +0300

    Moved hop config to user/hop.lua

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -30,9 +30,7 @@ lvim.plugins = {
         "phaazon/hop.nvim",
         event = "BufRead",
         config = function()
-            require("hop").setup()
-            vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-            vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+            require("user.hop").config()
         end,
     },
 }
