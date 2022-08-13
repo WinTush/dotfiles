655886c672d1958a3f81fd22ba817ee505b0e1c3
Winston Kinoti <prog.kinoti@gmail.com>
2022-08-13 15:59:45 +0300

    feat: show relativenumber in nvim-tree

--- lvim/config.lua
+++ lvim/config.lua
@@ -42,7 +42,8 @@ lvim.builtin.notify.active = true
 lvim.builtin.dap.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.view.number = true
+lvim.builtin.nvimtree.setup.view.relativenumber = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
