4000a296a483c21bc567fa5224b7e24a0cf09e4e
Winston Kinoti <prog.kinoti@gmail.com>
2022-08-13 20:46:27 +0300

    feat: add color highlighter

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -105,6 +105,20 @@ lvim.plugins = {
             require("user.wilder").config()
         end,
     },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
 }
 
 table.insert(lvim.builtin.cmp.sources, { name = "copilot", group_index = 0 })
