e58bad9de20c473dacf3f7fec966c00f369ffdad
Winston Kinoti <prog.kinoti@gmail.com>
2022-11-18 14:49:58 +0300

    update: use NvChad's colorizer

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -106,17 +106,26 @@ lvim.plugins = {
         end,
     },
     {
-        "norcalli/nvim-colorizer.lua",
+        "NvChad/nvim-colorizer.lua",
         config = function()
-            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
-                RGB = true, -- #RGB hex codes
-                RRGGBB = true, -- #RRGGBB hex codes
-                RRGGBBAA = true, -- #RRGGBBAA hex codes
-                rgb_fn = true, -- CSS rgb() and rgba() functions
-                hsl_fn = true, -- CSS hsl() and hsla() functions
-                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-            })
+            require("colorizer").setup {
+                filetypes = {
+                    "css",
+                    "scss",
+                    html = { names = false; mode = "foreground"; },
+                    "javascript",
+                },
+                user_default_options = {
+                    RGB = true, -- #RGB hex codes
+                    RRGGBB = true, -- #RRGGBB hex codes
+                    RRGGBBAA = true, -- #RRGGBBAA hex codes
+                    rgb_fn = true, -- CSS rgb() and rgba() functions
+                    hsl_fn = true, -- CSS hsl() and hsla() functions
+                    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+                    tailwind = true, -- Enable tailwind colors
+                },
+            }
         end,
     },
     {
